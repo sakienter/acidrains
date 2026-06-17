56d35631c6fdbf02aa9151b27854302892b6d6b1
sakienter <23mm0402@moon.aichi-u.ac.jp>
2026-06-18 02:36:37 +0900

    Add Tier 1 minion card module

---
+++ cards/minions/tier1.js
@@ -0,0 +1,11 @@
+/* acidcards.csv: Tier 1 minions and their effect overrides. */
+(() => {
+  window.AcidCardModules.register({
+    kind: 'minion',
+    tier: 1,
+    label: 'ティア1・ミニオン',
+    effects: {
+      // 'カード名': card => ({ battlecry(gameState) { ... } }),
+    },
+  });
+})();
