286346473c77f0e45b0ed1c985706bf632ce900b
sakienter <23mm0402@moon.aichi-u.ac.jp>
2026-06-17 21:17:49 +0900

    Load improved resource HUD

--- acidic_rain_cards.js
+++ acidic_rain_cards.js
@@ -39,3 +39,4 @@ document.write('<script defer src="./acidic_rain_spell_rules.js"><\/script>');
 document.write('<script defer src="./acidic_rain_drag_fix.js"><\/script>');
 document.write('<script defer src="./acidic_rain_engine_rules.js"><\/script>');
 document.write('<script defer src="./acidic_rain_turn_rules.js"><\/script>');
+document.write('<script defer src="./acidic_rain_hud_rules.js"><\/script>');
