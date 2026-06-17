c79a8aede16a9665462a6e3571242a964ea542eb
sakienter <23mm0402@moon.aichi-u.ac.jp>
2026-06-17 20:31:27 +0900

    Load final persistent stat rules

--- acidic_rain_cards.js
+++ acidic_rain_cards.js
@@ -1,8 +1,9 @@
 /*
  * Loader for the expanded Acidic Rain card set.
- * The immutable card-data snapshot is loaded first, then the rule extension
- * and compatibility hotfix are deferred until the main game engine exists.
+ * The immutable card-data snapshot is loaded first, then local rule modules
+ * are deferred until the main game engine exists.
  */
 document.write('<script src="https://cdn.jsdelivr.net/gh/sakienter/acidrains@eccf3cffd645d8d07df07e3056c1c0b2c42085fb/acidic_rain_cards.js"><\/script>');
 document.write('<script defer src="./acidic_rain_rules.js"><\/script>');
 document.write('<script defer src="./acidic_rain_hotfix.js"><\/script>');
+document.write('<script defer src="./acidic_rain_stat_rules.js"><\/script>');
