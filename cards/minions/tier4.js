492984b04c7d8cd5b41b7a9f39bca904139e5d78
sakienter <23mm0402@moon.aichi-u.ac.jp>
2026-06-18 23:46:43 +0900

    Register authoritative Tier 4 minions

--- cards/minions/tier4.js
+++ cards/minions/tier4.js
@@ -1,9 +1,12 @@
 (() => {
-  const modules = window.AcidCardModules;
-  modules.register({
+  const moduleData = window.AcidTier4Minions;
+  if (!moduleData) throw new Error('Tier 4 minion helpers are not loaded.');
+  window.AcidCardModules.register({
     kind: 'minion',
     tier: 4,
     label: 'ティア4・ミニオン',
-    effects: {},
+    definitions: moduleData.definitions,
+    effects: moduleData.effects,
+    apply: moduleData.apply,
   });
 })();
